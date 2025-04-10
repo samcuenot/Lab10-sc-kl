82e17249300419e0df94778ddc41e9ae3d0746e4
samanthacuenot <s.cuenot@ufl.edu>
2025-04-10 11:05:56 -0400

    fixed function calls again

--- test_calculator.py
+++ test_calculator.py
@@ -26,7 +26,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_divide(self): # 3 assertions
         self.assertEqual(div(2, 2), 1)
-        self.assertEqual(div(5, 10), 2)
+        self.assertEqual(div(5, 10), 0.5)
         self.assertEqual(div(5, 0), 0)
     # ##########################
 
